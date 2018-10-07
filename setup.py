b63db269e192a1663846e281d8eb08b1ce155df4
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-10-06 22:59:10 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,96 @@
+import discord
+import random
+import asyncio
+import time
+import datetime
+import sys
+import io
+import os
+import re
+import json
+import base64
+
+client = discord.Client()
+
+COR = 0x3498DB
+
+@client.event
+async def on_ready():
+    print("Iniciado com sucesso!")
+    while True:
+        await client.change_presence(game=discord.Game(name="Online com mais de {} membros!".format(str(len(set(client.get_all_members())))), url="https://www.twitch.tv/johncostaxv", type=1))
+        await asyncio.sleep(300)
+        await client.change_presence(game=discord.Game(name="Criando tickets!", url="https://www.twitch.tv/johncostaxv", type=1))
+        await asyncio.sleep(300)
+        await client.change_presence(game=discord.Game(name="Criado pelo Johnn#0001", url="https://www.twitch.tv/johncostaxv", type=1))
+        await asyncio.sleep(300)
+
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith("!criarticket"):
+        cargos = [
+            # IDs dos cargos:
+            "412708220021506058", #Master
+        ]
+        for r in message.author.roles:
+            if r.id in cargos:
+
+                ticket = discord.Embed(
+                    color=COR,
+                    description="Para solicitar suporte, clique na reação de sua dúvida.\n\n"
+                                "`💎 - Compras`\n"
+                                "`📋 - Aplicações`\n"
+                                "`⛔ - Punições`\n"
+                                "`🎳 - Outros"
+                )
+                ticket.set_author(name="Sistema de suporte")
+                ticket.set_footer(text="Equipe de desenvolvimento do discord", icon_url="https://images-ext-1.discordapp.net/external/BCKxPNzZzEVfkbIublv7_3wG2016jTwGk3onTemVRnM/%3Fv%3D1/https/cdn.discordapp.com/emojis/450112878108999680.gif")
+                ticket.timestamp = datetime.datetime.utcnow()
+
+                react = await client.send_message(message.channel, embed=ticket)
+                
+                await client.add_reaction(react, "💎")
+                await client.add_reaction(react, "📋")
+                await client.add_reaction(react, "⛔")
+                await client.add_reaction(react, "🎳")
+
+                global msg_id
+                msg_id = react.id
+
+                global msg_user
+                msg_user = message.author
+
+@client.event
+async def on_reaction_add(reaction, user):
+    msg = reaction.message
+
+    if reaction.emoji == "💎" and msg.id == msg_id: #and user == msg_user:
+     for role in user.roles:
+         if role.name == "👨🏻‍🚀 Jogador":
+             await client.remove_reaction(msg, "💎", user)
+
+     print("funciono")
+
+    if reaction.emoji == "📋" and msg.id == msg_id: #and user == msg_user:
+     for role in user.roles:
+         if role.name == "👨🏻‍🚀 Jogador":
+             await client.remove_reaction(msg, "📋", user)
+
+     print("funciono")
+
+    if reaction.emoji == "⛔" and msg.id == msg_id: #and user == msg_user:
+     for role in user.roles:
+         if role.name == "👨🏻‍🚀 Jogador":
+             await client.remove_reaction(msg, "⛔", user)
+
+     print("funciono")
+
+
+
+    if reaction.emoji == "🎳" and msg.id == msg_id: #and user == msg_user:
+     for role in user.roles:
+         if role.name == "👨🏻‍🚀 Jogador":
+             await client.remove_reaction(msg, "🎳", user)
+
+
+client.run(os.environ.get("BOT_TOKEN"))
